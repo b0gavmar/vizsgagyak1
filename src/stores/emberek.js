f21ff723270a43a5af2578903edbab9f65187c4b
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-18 01:24:23 +0200

    emberek betoltese storebol es oldalo router meg view meg store

---
+++ src/stores/emberek.js
@@ -0,0 +1,16 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios';
+
+const url = "http://localhost:3000"
+
+export const useEmberStore = defineStore('ember', () => {
+  const emberek = ref([]);
+
+  const fetchAll = async() => {
+    const resp = await axios.get(url+"/emberek")
+    emberek.value = resp
+  }
+
+  return { emberek, fetchAll }
+})
---
+++ src/views/EmberekView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Emberek</h1>
+  </div>
+</template>
+
+<style scoped></style>
+
+<script setup>
+import { useEmberStore } from "@/stores/emberek";
+import { onMounted } from "vue";
+
+const emberStore = useEmberStore();
+
+onMounted(() => {
+  emberStore.fetchAll();
+});
+</script>
