725bb1dbd0de981aa6344e515801384f2a514746
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-18 15:20:33 +0200

    form

--- src/views/AddNew.vue
+++ src/views/AddNew.vue
@@ -1,8 +1,30 @@
 <script setup>
+import { useToast } from 'vue-toastification';
+import { useEmberStore } from '@/stores/emberek';
+import { ref } from 'vue'
+
+const emberstore = useEmberStore()
+
+const newEmber = ref({
+  "id": emberstore.getNextId(),
+  "nev": "",
+  "kor": "",
+  "nem": "",
+})
+
 </script>
 
 <template>
-  <main>
-    Otthon
-  </main>
+  <div class="container">
+    <label for="nev" class="form-label">Név</label>
+    <input type="text" name="nev" id="nev" class="form-control" v-model="newEmber.nev">
+
+    <label for="kor" class="form-label">Kor</label>
+    <input type="text" name="kor" id="kor" class="form-control" v-model="newEmber.kor">
+
+    <label for="nem" class="form-label">Nem</label>
+    <input type="text" name="nrm" id="nem" class="form-control">
+
+    <button class="btn btn-primary mt-2">Hozzáad</button>
+  </div>
 </template>
